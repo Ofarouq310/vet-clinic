48f1ad539c44e5582131b1fcb6da7d97b29b1ca4
Omar Farouq <ofarouq310@gmail.com>
2023-05-26 10:43:40 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -7,5 +7,5 @@ CREATE TABLE animals (
     escape_attempts INT NOT NULL,
     neutered BOOLEAN NOT NULL,
     weight_kg DECIMAL NOT NULL,
-    ALTER TABLE animals ADD species varchar(50);
+    species VARCHAR(50);
 );
